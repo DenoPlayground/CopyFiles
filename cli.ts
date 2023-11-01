9c26e361bad6468df266a5270b1f75366d312574
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-11-01 17:32:48 +0100

    Update cli.ts

--- cli.ts
+++ cli.ts
@@ -1,1 +1,12 @@
+import { copy } from "./src/copy.ts"
 
+const args = parse(Deno.args)
+
+const srcPath = args._[0].toString() ?? './'
+const distPath = args._[1].toString() ?? './dist/'
+const exts = (args.exts as string).split(',')
+
+copy(srcPath, distPath, {
+  extensions: exts,
+  
+})
